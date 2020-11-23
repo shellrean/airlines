70a4b23eb7be1aefb53b6092404745c1e89d1c3d
shellrean <reanyusufali@gmail.com>
2020-11-23 09:19:51 +0700

    (domain): Create domain for airport

---
+++ domain/airport.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+    "context"
+    "time"
+)
+
+// Airport...
+type Airport struct {
+    ID          int64       `json:"id"`
+    Code        string      `json:"code"`
+    Name        string      `json:"name"`
+    CreatedAt   time.Time   `json:"-"`
+    UpdatedAt   time.Time   `json:"-"`
+}
+
+// AirportRepository represent airport's repository
+type AirportRepository interface {
+    Fetch(ctx context.Context, num int64) ([]Airport, error)
+}
+
+// AirportUsecase represent airport's usecase
+type AirportUsecase interface {
+
+}
